539f416cf19f165dcd9e6b9c5f2465b0ba00c500
Dan Parry <parryd424@gmail.com>
2017-04-08 20:42:39 -0400

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,12 @@
+module MetaDancing
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
+
+# class Kid
+#   extend MetaDancing
+# end
+# class Dancer
+#   extend MetaDancing
+# end
